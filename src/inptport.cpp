a4637fd15a2a5e829d2cc87e5c0ee49713441ac9
Steve Maddison <steve@cosam.org>
2010-09-06 19:29:35 +0000

    Fixed Pandora button bug. Temporary dirs.rc in PND to avoid non-working path config.

--- src/inptport.cpp
+++ src/inptport.cpp
@@ -133,9 +133,9 @@ char ipdn_defaultstrings[][MAX_DEFSTR_LEN] =
 struct ipd inputport_defaults[] =
 {
 #ifdef PANDORA
-	{ IPT_UI_CONFIGURE,         "Config Menu",       SEQ_DEF_1(KEYCODE_TAB) },
-#else
 	{ IPT_UI_CONFIGURE,         "Config Menu",       SEQ_DEF_3(KEYCODE_TAB, CODE_OR, KEYCODE_MENU) },
+#else
+	{ IPT_UI_CONFIGURE,         "Config Menu",       SEQ_DEF_1(KEYCODE_TAB) },
 #endif
 	{ IPT_UI_ON_SCREEN_DISPLAY, "On Screen Display", SEQ_DEF_1(KEYCODE_TILDE) },
 	{ IPT_UI_PAUSE,             "Pause",             SEQ_DEF_1(KEYCODE_P) },
